171d1fdd412d0180a0dcc619a853c02e57946a5b
mplik <biuro@mplik.pl>
2024-12-30 15:02:50 +0100

    Dodano nowe pliki menu.js zmodyfikowano plik feature-branch-one podfoldern/scripts.js index.html

--- podfoldern/scripts.js
+++ podfoldern/scripts.js
@@ -1,11 +1,29 @@
 function changeImage() {
-    var image1 = document.getElementById('image1');
-    var image2 = document.getElementById('image2');
+    const image1 = document.getElementById('image1');
+    const image2 = document.getElementById('image2');
     if (image1.style.display === 'none') {
         image1.style.display = 'block';
         image2.style.display = 'none';
     } else {
         image1.style.display = 'none';
         image2.style.display = 'block';
+    }
+}
+
+document.getElementById('nav-toggle').addEventListener('click', function() {
+    const menu = document.getElementById('nav-menu');
+    if (menu.style.display === 'block') {
+        menu.style.display = 'none';
+    } else {
+        menu.style.display = 'block';
+    }
+});
+
+function toggleCard() {
+    const content = document.querySelector('.card-content');
+    if (content.style.display === 'block') {
+        content.style.display = 'none';
+    } else {
+        content.style.display = 'block';
     }
 }
